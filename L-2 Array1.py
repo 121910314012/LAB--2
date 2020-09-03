73a6a7cd962fc6345a1b8fa48be4602f1d9049df
121910314012 <69962575+121910314012@users.noreply.github.com>
2020-09-03 15:16:58 +0530

    Create L-2 Array1.py

---
+++ L-2 Array1.py
@@ -0,0 +1,33 @@
+# COPY THE CONTENTS FROM ONE ARRAY TO ANOTHER ARRAY
+#Intialize the array
+ar1=input(" enter the content :")
+# Another array ar2 wiht same size of ar1
+ar2=[None]*len(ar1)
+length=len(ar1)
+#Copy elements from one array to another
+for i in range(0,length):
+    ar2[i]=ar1[i]
+ #Display elements of ar1
+print("elements in original array : ")
+print(ar1)
+print()
+#Display elements of aar2
+print("elements in new array : ")
+for i in range(0,len(ar2)):
+    print(ar2[i])
+   
+   OUTPUT:
+    enter the content :1 2 3 4 5
+elements in original array : 
+1 2 3 4 5
+
+elements in new array : 
+1
+ 
+2
+ 
+3
+ 
+4
+ 
+5
